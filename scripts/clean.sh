4971bec4bd71b0f88509885354578a758418576f
andrew <porcherface@gmail.com>
2021-01-05 12:58:21 +0100

    finished clean

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -8,8 +8,12 @@ KEY2=".DS_Store"
 k1list=$(find . -name $KEY1)
 k2list=$(find . -name $KEY2)
 
+rm -rf $k1list
+rm -rf $k2list
+
+# git rm (if necessary
 git rm $k1list -rf
-git rm $k2list
+git rm $k2list -rf
 
 
 
